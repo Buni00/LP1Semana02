8d34c1c218b355df4449ae8e12297ff0753286c3
bruno-hrf <rodrigues_bruno@outlook.pt>
2023-03-09 18:59:16 +0000

    Add variables

---
+++ PerguntaValores/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PerguntaValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a; 
+            double r; 
+            double Volume;
+            double AreaSup;
+        }
+    }
+}
