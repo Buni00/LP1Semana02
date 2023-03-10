5f34a213ec3a4e1a37582a841fee6f1843b00b56
Bruno Fernandes <a22103346@alunos.ulht.pt>
2023-03-10 14:57:30 +0000

    Add first diagonal X

--- CrossSquare/Program.cs
+++ CrossSquare/Program.cs
@@ -17,26 +17,46 @@ namespace CrossSquare
 
             for(int i=0; i<n-2; i++)
             {
+                
                 for(int j = 1; j<=n; j++)
-                {
+                {          
                     if(j==1)
                     {
                         Console.Write("#");
                     }
-                    if(j==n-1)
+                    if(j==1 && i==0)
+                    {
+                        Console.Write("X");
+                    }
+                    if(j==2 && i==1)
+                    {
+                        Console.Write("X");
+                    }
+                    if(j==3 && i==2)
+                    {
+                        Console.Write("X");
+                    }
+                    if(j==4 && i==3)
+                    {
+                        Console.Write("X");
+                    }
+                    if(j==5 && i==4)
+                    {
+                        Console.Write("X");
+                    }
+                    if(j==n-2)
                     {
                         Console.Write("#");
                     }
+                    
                     else
                     {
                         Console.Write(" ");
                     }
                 }
-                Console.WriteLine("");
-
+                Console.WriteLine(""); 
+                    
             }
-
-
 
             for (int i = 0; i<n ; i++)
             {
