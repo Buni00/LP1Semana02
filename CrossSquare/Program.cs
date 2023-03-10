22712f4569efa7d2f5a4a51cd645a98f6a5e86d5
Bruno Fernandes <a22103346@alunos.ulht.pt>
2023-03-10 14:23:27 +0000

    Add sides of square

--- CrossSquare/Program.cs
+++ CrossSquare/Program.cs
@@ -14,7 +14,29 @@ namespace CrossSquare
             }            
             Console.WriteLine("");
 
-            for int i= 0; i<n-2; i++)
+
+            for(int i=0; i<n-2; i++)
+            {
+                for(int j = 1; j<=n; j++)
+                {
+                    if(j==1)
+                    {
+                        Console.Write("#");
+                    }
+                    if(j==n-1)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine("");
+
+            }
+
+
 
             for (int i = 0; i<n ; i++)
             {
