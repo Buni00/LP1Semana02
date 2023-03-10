7836cb8e094b968cddf33e2b20e617794c562e07
Bruno Fernandes <a22103346@alunos.ulht.pt>
2023-03-10 13:57:53 +0000

    Add 2 for cycles

---
+++ Triangle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = Convert.ToInt32(args[0]);
+            string letra = args[1];
+
+            for (int i = 0; i < numero; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(letra);
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
