6fd7291ce1a7c452443af8d79e2bca02abb1eb07
Bruno Fernandes <a22103346@alunos.ulht.pt>
2023-03-10 14:08:42 +0000

    Add upper square line and bottom square line

---
+++ CrossSquare/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CrossSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(args[0]);
+
+            for (int i = 0; i<n ; i++)
+            {
+                Console.Write("#");
+            }            
+            Console.WriteLine("");
+
+            for int i= 0; i<n-2; i++)
+
+            for (int i = 0; i<n ; i++)
+            {
+                Console.Write("#");
+            }
+        }
+    }
+}
