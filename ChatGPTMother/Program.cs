8064be7a1ad76aa7e9290f4e5488fc3565f71f6f
bruno-hrf <rodrigues_bruno@outlook.pt>
2023-03-10 01:04:24 +0000

    Add 4 questions

---
+++ ChatGPTMother/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ChatGPTMother
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string question1 = "Whats your name?";
+            string question2 = "1+1?";
+            string question3 = "2+2?";
+            string question4 = "2+1?";
+
+            while(true)
+            {
+                Console.WriteLine("Ask me a question");
+                string answer = Console.ReadLine();
+
+                if (answer == question1)
+                    Console.WriteLine("I'm ChatGPTMother \n");
+
+                else if (answer == question2)
+                    Console.WriteLine("2 \n");
+
+                else if (answer == question3)
+                    Console.WriteLine("4 \n");    
+
+                else if (answer == question4)
+                    Console.WriteLine("3 \n");    
+                
+                else
+                    Console.WriteLine("Dont know the answer \n");
+
+
+            }
+            
+        
+            
+            
+        }
+    }
+}
